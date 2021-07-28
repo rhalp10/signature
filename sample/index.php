edc05ee3fcb5c86830aa279dbaa3c279eff2c631
Rhalp Darren Cabrera <rhalpdarrencabrera@gmail.com>
2021-07-28 19:42:28 +0800

    Added: Touchscreen signature is now working on phones/tablets.
    - Added: update function for person signature on dataTable.
    - Added: Touchscreen signature is now working on phones/tablets.

--- sample/index.php
+++ sample/index.php
@@ -70,7 +70,10 @@
                                 <label for="inputSignatureName">Name</label>
                                 <input type="text" class="form-control" id="inputSignatureName" name="signatureName" required="">
                             </div>
-                            <button class="btn btn-outline-success" id="submit">SUBMIT</button>
+                            <input type="hidden" id="inputSignatureID" name="signatureID">
+                            <input type="hidden" id="action" name="action">
+                            <button class="btn btn-outline-success" id="submitSignature">SUBMIT</button>
+                            <button class="btn btn-outline-primary" id="updateSignature">UPDATE</button>
                         </form>
                     </div>
                     <div class="col-md-6 table-responsive">
@@ -109,127 +112,81 @@
         <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
         <script src="../assets/js/jquery.signature.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui-touch-punch/0.2.3/jquery.ui.touch-punch.min.js"></script>
+        
 
         <script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
         <script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js"></script>
 
         <script>
             $(function() {
+                var b4sign = {
+                dataTable:null,
+                container: $('#signatureContainer').signature({ color: '#0080FF' }),
+                initializeDatatable: function() {
+                    this.dataTable = $('#signatureTable').DataTable({
+                        "processing": true,
+                        "serverSide": true,
+                        "order": [],
+                        "ajax": {
+                            url: "fetchTable.php",
+                            type: "POST"
 
-                var signatureTable = $('#signatureTable').DataTable({
-                    "processing": true,
-                    "serverSide": true,
-                    "order": [],
-                    "ajax": {
-                        url: "fetchTable.php",
-                        type: "POST"
-
-                    },
-                    "columnDefs": [{
-                        "targets": [0],
-                        "orderable": false,
-                    }, ],
-
-                });
-                var signatureContainer = $('#signatureContainer').signature();
-                $('#disable').click(function() {
-                    var disable = $(this).text() === 'Disable';
-                    $(this).text(disable ? 'Enable' : 'Disable');
-                    signatureContainer.signature(disable ? 'disable' : 'enable');
-                });
-                $('#clear').click(function() {
-                    signatureContainer.signature('clear');
-                });
-                $('#json').click(function() {
-                    if (signatureContainer.signature('isEmpty')) {
-                        return alert("Signature Canvas Is Empty");
-                    }
-
-                    $.ajax({
-                        url: "action.php",
-                        method: 'POST',
-                        data: {
-                            action: "saveJson",
-                            signature: signatureContainer.signature('toJSON')
                         },
-                        dataType: 'json',
-                        success: function(data) {
-                            alert(data.msg);
-                            signatureTable.ajax.reload();
-                        }
+                        "columnDefs": [{
+                            "targets": [0],
+                            "orderable": false,
+                        }, ],
                     });
-
-                });
-                $('#svg').click(function() {
-
-                    if (signatureContainer.signature('isEmpty')) {
-                        return alert("Signature Canvas Is Empty");
+                },
+                disableContainer: function(selector){
+                    var disable = $(selector).text() === 'Disable';
+                    $(selector).text(disable ? 'Enable' : 'Disable');
+                    this.container.signature(disable ? 'disable' : 'enable');
+                },
+                checkContainer:function(){
+                    if (this.container.signature('isEmpty')) {
+                         alert("Signature Canvas Is Empty");
+                         return true;
                     }
-
+                    return false;
+                },
+                svgSave:function(){
+                    if (this.checkContainer()) return;
                     $.ajax({
                         url: "action.php",
                         method: 'POST',
                         data: {
                             action: "saveSVG",
-                            signature: signatureContainer.signature('toSVG')
+                            signature: this.container.signature('toSVG')
                         },
                         dataType: 'json',
                         success: function(data) {
                             alert(data.msg);
-
-                            signatureTable.ajax.reload();
+                            b4sign.container.signature('clear');
+                            b4sign.dataTable.ajax.reload();
                         }
                     });
-
-                });
-                signatureContainer.signature({
-                    color: '#0080FF'
-                });
-
-                $(document).on('click', '#loadSignature', function() {
-                    var person_ID = $(this).attr("data-id");
+                },
+                jsonSave: function(){
+                    if (this.checkContainer()) return;
                     $.ajax({
                         url: "action.php",
                         method: 'POST',
                         data: {
-                            action: "load",
-                            "person_ID": person_ID
-                        },
-                        dataType: 'json',
-                        success: function(data) {
-                            signatureContainer.signature('draw', data.Signature);
-                        }
-                    });
-
-                });
-               $(document).on('click', '#deleteSignature', function() {
-                var person_ID = $(this).attr("data-id");
-                   $.ajax({
-                        url: "action.php",
-                        method: 'POST',
-                        data: {
-                            action: "delete",
-                            "person_ID": person_ID
+                            action: "saveJson",
+                            signature: this.container.signature('toJSON')
                         },
                         dataType: 'json',
                         success: function(data) {
                             alert(data.msg);
-                            signatureTable.ajax.reload();
+                            b4sign.container.signature('clear');
+                            b4sign.dataTable.ajax.reload();
                         }
                     });
-                });
-                
-
-                $(document).on('submit', '#signatureForm', function(event) {
-                    event.preventDefault();
-                    var formData = new FormData(this);
-                    if (signatureContainer.signature('isEmpty')) {
-
-                        return alert("Signature  Is required");
-
-                    }
-                    formData.append("action", "submit");
-                    formData.append("signature", signatureContainer.signature('toJSON'));
+                },
+                formSave:function(formData){
+                    formData.append("signature", b4sign.container.signature('toJSON'));
                        $.ajax({
                         url: "action.php",
                         method: 'POST',
@@ -239,12 +196,79 @@
                         dataType: 'json',
                         success: function(data) {
                             alert(data.msg);
-                            signatureTable.ajax.reload();
+                            b4sign.dataTable.ajax.reload();
                         }
                     });
+                },
+                load:function(person_ID){
+                    $('#inputSignatureID').val(person_ID);
+                    $.ajax({
+                        url: "action.php",
+                        method: 'POST',
+                        data: {
+                            action: "load",
+                            "person_ID": person_ID
+                        },
+                        dataType: 'json',
+                        success: function(data) {
+                            $('#inputSignatureName').val(data.Name);
+                            b4sign.container.signature('draw', data.Signature);
+                        }
+                    });
+                },
+                delete:function(person_ID){
+                    $.ajax({
+                        url: "action.php",
+                        method: 'POST',
+                        data: {
+                            action: "delete",
+                            "person_ID": person_ID
+                        },
+                        dataType: 'json',
+                        success: function(data) {
+                            alert(data.msg);
+                            b4sign.dataTable.ajax.reload();
+                        }
+                    });
+                }};
+                b4sign.initializeDatatable();
+                b4sign.container;
+                
+                $('#disable').click(function() {
+                    b4sign.disableContainer(this);
                 });
-
+                $('#clear').click(function() {
+                    b4sign.container.signature('clear');
+                });
+                $(document).on('click', '#loadSignature', function() {
+                    b4sign.load($(this).attr("data-id"));
+                });
+                
+                $(document).on('click', '#submitSignature', function() {
+                    $('#action').val('submit');
+                });
+                $(document).on('click', '#updateSignature', function() {
+                    $('#action').val('update');
+                });
+                $(document).on('click', '#deleteSignature', function() {
+                    b4sign.delete($(this).attr("data-id"));
+                });
+                $('#json').click(function() {
+                    b4sign.jsonSave();
+                });
+                $('#svg').click(function() {
+                    b4sign.svgSave();
+                });
+                $(document).on('submit', '#signatureForm', function(event) {
+                    event.preventDefault(); 
+                    if (b4sign.checkContainer()) return;
+                    b4sign.formSave(new FormData(this));
+                });
+                window.addEventListener('scroll', function(e) {
+                    console.log('scrolling');
+                });
             });
+           
         </script>
 
     </body>
